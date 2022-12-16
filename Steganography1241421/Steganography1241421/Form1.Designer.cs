d10c0491104c2cae42526391304ebdd3c5d5127b
kriSasaki <99545732+kriSasaki@users.noreply.github.com>
2022-12-16 16:28:49 +0300

    Add files via upload

---
+++ Steganography1241421/Steganography1241421/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace Steganography1241421
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.image = new System.Windows.Forms.PictureBox();
+            this.uploadImage = new System.Windows.Forms.Button();
+            this.messageBox = new System.Windows.Forms.RichTextBox();
+            this.enscryptButton = new System.Windows.Forms.Button();
+            this.descryptedMessage = new System.Windows.Forms.RichTextBox();
+            this.descryptImage = new System.Windows.Forms.Button();
+            this.ClearImage = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.image)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // image
+            // 
+            this.image.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.image.Location = new System.Drawing.Point(14, 41);
+            this.image.Name = "image";
+            this.image.Size = new System.Drawing.Size(562, 328);
+            this.image.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.image.TabIndex = 0;
+            this.image.TabStop = false;
+            this.image.Click += new System.EventHandler(this.image_Click);
+            // 
+            // uploadImage
+            // 
+            this.uploadImage.Location = new System.Drawing.Point(14, 12);
+            this.uploadImage.Name = "uploadImage";
+            this.uploadImage.Size = new System.Drawing.Size(172, 23);
+            this.uploadImage.TabIndex = 1;
+            this.uploadImage.Text = "Загрузить картинку";
+            this.uploadImage.UseVisualStyleBackColor = true;
+            this.uploadImage.Click += new System.EventHandler(this.UploadImage_Click);
+            // 
+            // messageBox
+            // 
+            this.messageBox.Location = new System.Drawing.Point(14, 404);
+            this.messageBox.Name = "messageBox";
+            this.messageBox.Size = new System.Drawing.Size(562, 96);
+            this.messageBox.TabIndex = 2;
+            this.messageBox.Text = "";
+            // 
+            // enscryptButton
+            // 
+            this.enscryptButton.Location = new System.Drawing.Point(14, 375);
+            this.enscryptButton.Name = "enscryptButton";
+            this.enscryptButton.Size = new System.Drawing.Size(145, 23);
+            this.enscryptButton.TabIndex = 3;
+            this.enscryptButton.Text = "Вставить текст";
+            this.enscryptButton.UseVisualStyleBackColor = true;
+            this.enscryptButton.Click += new System.EventHandler(this.EnscryptButton_Click);
+            // 
+            // descryptedMessage
+            // 
+            this.descryptedMessage.Location = new System.Drawing.Point(582, 41);
+            this.descryptedMessage.Name = "descryptedMessage";
+            this.descryptedMessage.Size = new System.Drawing.Size(286, 328);
+            this.descryptedMessage.TabIndex = 4;
+            this.descryptedMessage.Text = "";
+            this.descryptedMessage.TextChanged += new System.EventHandler(this.descryptedMessage_TextChanged);
+            // 
+            // descryptImage
+            // 
+            this.descryptImage.Location = new System.Drawing.Point(582, 12);
+            this.descryptImage.Name = "descryptImage";
+            this.descryptImage.Size = new System.Drawing.Size(165, 23);
+            this.descryptImage.TabIndex = 5;
+            this.descryptImage.Text = "Получить сообщение";
+            this.descryptImage.UseVisualStyleBackColor = true;
+            this.descryptImage.Click += new System.EventHandler(this.DescryptImage_Click);
+            // 
+            // ClearImage
+            // 
+            this.ClearImage.Location = new System.Drawing.Point(192, 12);
+            this.ClearImage.Name = "ClearImage";
+            this.ClearImage.Size = new System.Drawing.Size(145, 23);
+            this.ClearImage.TabIndex = 6;
+            this.ClearImage.Text = "Очистить картинку";
+            this.ClearImage.UseVisualStyleBackColor = true;
+            this.ClearImage.Click += new System.EventHandler(this.ClearImage_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.ClientSize = new System.Drawing.Size(883, 514);
+            this.Controls.Add(this.ClearImage);
+            this.Controls.Add(this.descryptImage);
+            this.Controls.Add(this.descryptedMessage);
+            this.Controls.Add(this.enscryptButton);
+            this.Controls.Add(this.messageBox);
+            this.Controls.Add(this.uploadImage);
+            this.Controls.Add(this.image);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.image)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox image;
+        private System.Windows.Forms.Button uploadImage;
+        private System.Windows.Forms.RichTextBox messageBox;
+        private System.Windows.Forms.Button enscryptButton;
+        private System.Windows.Forms.RichTextBox descryptedMessage;
+        private System.Windows.Forms.Button descryptImage;
+        private System.Windows.Forms.Button ClearImage;
+    }
+}
+
---
+++ Steganography1241421/Steganography1241421/Form1.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Steganography1241421
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public enum State
+        {
+            Hiding,
+            Filling_With_Zeros
+        };
+
+        public static Bitmap EmbedText(string text, Bitmap bmp)
+        {
+            State state = State.Hiding;
+            int charIndex = 0;
+            int charValue = 0;
+            long pixelElementIndex = 0;
+            int zeros = 0;
+            int R = 0, G = 0, B = 0;
+
+            for (int i = 0; i < bmp.Height; i++)
+            {
+                for (int j = 0; j < bmp.Width; j++)
+                {
+                    Color pixel = bmp.GetPixel(j, i);
+
+                    R = pixel.R - pixel.R % 2;
+                    G = pixel.G - pixel.G % 2;
+                    B = pixel.B - pixel.B % 2;
+
+                    for (int n = 0; n < 3; n++)
+                    {
+                        if (pixelElementIndex % 8 == 0)
+                        {
+                            if (state == State.Filling_With_Zeros && zeros == 8)
+                            {
+                                if ((pixelElementIndex - 1) % 3 < 2)
+                                {
+                                    bmp.SetPixel(j, i, Color.FromArgb(R, G, B));
+                                }
+
+                                return bmp;
+                            }
+
+                            if (charIndex >= text.Length)
+                            {
+                                state = State.Filling_With_Zeros;
+                            }
+                            else
+                            {
+                                charValue = text[charIndex++];
+                            }
+                        }
+
+                        switch (pixelElementIndex % 3)
+                        {
+                            case 0:
+                                {
+                                    if (state == State.Hiding)
+                                    {
+                                        R += charValue % 2;
+
+                                        charValue /= 2;
+                                    }
+                                }
+                                break;
+                            case 1:
+                                {
+                                    if (state == State.Hiding)
+                                    {
+                                        G += charValue % 2;
+
+                                        charValue /= 2;
+                                    }
+                                }
+                                break;
+                            case 2:
+                                {
+                                    if (state == State.Hiding)
+                                    {
+                                        B += charValue % 2;
+
+                                        charValue /= 2;
+                                    }
+
+                                    bmp.SetPixel(j, i, Color.FromArgb(R, G, B));
+                                }
+                                break;
+                        }
+
+                        pixelElementIndex++;
+
+                        if (state == State.Filling_With_Zeros)
+                        {
+                            zeros++;
+                        }
+                    }
+                }
+            }
+
+            return bmp;
+        }
+
+        public static string ExtractText(Bitmap bmp)
+        {
+            int colorUnitIndex = 0;
+            int charValue = 0;
+
+            string extractedText = String.Empty;
+
+            for (int i = 0; i < bmp.Height; i++)
+            {
+                for (int j = 0; j < bmp.Width; j++)
+                {
+                    Color pixel = bmp.GetPixel(j, i);
+
+                    for (int n = 0; n < 3; n++)
+                    {
+                        switch (colorUnitIndex % 3)
+                        {
+                            case 0:
+                                {                                  
+                                    charValue = charValue * 2 + pixel.R % 2;
+                                }
+                                break;
+                            case 1:
+                                {
+                                    charValue = charValue * 2 + pixel.G % 2;
+                                }
+                                break;
+                            case 2:
+                                {
+                                    charValue = charValue * 2 + pixel.B % 2;
+                                }
+                                break;
+                        }
+
+                        colorUnitIndex++;
+
+                        if (colorUnitIndex % 8 == 0)
+                        {
+                            charValue = ReverseBits(charValue);
+                            
+                            if (charValue == 0)
+                            {
+                                return extractedText;
+                            }
+                            
+                            char c = (char)charValue;
+                            extractedText += c.ToString();
+                        }
+                    }
+                }
+            }
+
+            return extractedText;
+        }
+
+        public static int ReverseBits(int n)
+        {
+            int result = 0;
+
+            for (int i = 0; i < 8; i++)
+            {
+                result = result * 2 + n % 2;
+
+                n /= 2;
+            }
+
+            return result;
+        }
+
+        private void UploadImage_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image Files(*.BMP)|*.BMP";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    image.Image = new Bitmap(openFileDialog.FileName);
+                }
+                catch
+                {
+                    MessageBox.Show("Невозможно открыть данный файл", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+
+        private void EnscryptButton_Click(object sender, EventArgs e)
+        {
+            if (messageBox.Text != null)
+            {
+                string message = messageBox.Text;
+
+                if (image.Image != null)
+                {
+                    Bitmap enscryptedImage = EmbedText(message, (Bitmap)image.Image);
+                    SaveFileDialog saveFileDialog = new SaveFileDialog();
+                    saveFileDialog.Title = "Сохранить картинку как...";
+                    saveFileDialog.OverwritePrompt = true;
+                    saveFileDialog.CheckPathExists = true;
+                    saveFileDialog.Filter = "Image Files(*.BMP)|*.BMP";
+                    saveFileDialog.ShowHelp = true;
+
+                    if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        try
+                        {
+                            enscryptedImage.Save(saveFileDialog.FileName);
+                        }
+                        catch
+                        {
+                            MessageBox.Show("Невозможно сохранить изображение", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                    }
+                }
+                else
+                {
+                    descryptedMessage.Text = "Отсутствует картинка.";
+                }
+            }
+            else
+            {
+                descryptedMessage.Text = "Отсутствует текст.";
+            }
+        }
+
+
+        private void DescryptImage_Click(object sender, EventArgs e)
+        {
+            if (image.Image != null)
+            {
+                descryptedMessage.Text = ExtractText((Bitmap)image.Image);
+            }
+            else
+            {
+                descryptedMessage.Text = "Отсутствует картинка.";
+            }
+        }
+
+        private void ClearImage_Click(object sender, EventArgs e)
+        {
+            image.Image = null;
+        }
+
+        private void descryptedMessage_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void image_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
